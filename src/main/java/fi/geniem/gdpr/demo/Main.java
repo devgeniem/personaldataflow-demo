6eeadfa9489c1ce7dfd0934aa686fc9240184cff
Kalle Hjerppe <kalle.hjerppe@solita.fi>
2017-04-28 17:14:51 +0300

    some work on validation rules

--- src/main/java/fi/geniem/gdpr/demo/Main.java
+++ src/main/java/fi/geniem/gdpr/demo/Main.java
@@ -10,28 +10,29 @@ public class Main {
 	@PersonalData
 	public static final String foo2 = "bar";
 	
+	public static final PersonalDataModel foo3 = new PersonalDataModel();
+	
 	public static void main(String[] args){
 		
-		//WARN: personalData in unsafe method
+		//WARN
 		PersonalDataModel foo = new PersonalDataModel();
 		
 		//OK
 		safeMethod(foo);
 
+		//WARN
 		unsafeMethod(foo);
 	}
 	
 	
-	private static void unsafeMethod(PersonalDataModel foo /*WARN: personalData parameter in unsafe */){
+	private static void unsafeMethod(PersonalDataModel foo /*WARN */){
 		
 	}
 	
 	@PersonalDataHandler
-	private static void safeMethod(PersonalDataModel foo){
-		
+	private static void safeMethod(PersonalDataModel foo /*OK*/){
 		//OK
 		PersonalDataModel bar = new PersonalDataModel();
-		
 		bar.getIdentityNumber();
 	}
 	
